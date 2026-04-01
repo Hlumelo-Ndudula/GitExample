b4f30769ceb40063b6ff61ff5b9fc6f6f0d22ee4
Student <Student@LabVM2047644.isgpl30zdifudebo0gsw0ixpkf.jnbx.internal.cloudapp.net>
2026-04-01 10:14:54 +0200

    My first comment

---
+++ src/main/java/com/mycompany/github/Github.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.github;
+
+/**
+ *
+ * @author Student
+ */
+public class Github {
+
+    public static void main(String[] args) {
+
+    }
+}
